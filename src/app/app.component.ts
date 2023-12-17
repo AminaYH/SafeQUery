5c5129929b8b8faaf8bf8271098921def39dd375
amina <amina.yahia@eniso.u-sousse.tn>
2023-12-17 20:42:46 +0100

    [FC] first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {
+  NgbInputDatepicker,
+  NgbNav,
+  NgbNavContent,
+  NgbNavItem,
+  NgbNavLinkBase,
+  NgbNavModule,
+  NgbNavOutlet
+} from '@ng-bootstrap/ng-bootstrap';
+import {TooltipModule} from "ngx-bootstrap/tooltip";
+import {NavbarComponent} from "./navbar/navbar.component";
+import {SidebarComponent} from "./sidebar/sidebar.component";
+import {UploadComponent} from "./upload/upload.component";
 
 @Component({
+
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NgbNav, NgbNavItem, NgbNavOutlet, NgbNavLinkBase, NgbNavContent, NgbInputDatepicker, TooltipModule, NavbarComponent, SidebarComponent, UploadComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'untitled2';
+  active = 1;
+
 }
