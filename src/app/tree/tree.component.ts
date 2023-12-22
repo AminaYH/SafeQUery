ec3d57b066bec700aa55b90943f96fbdeafa322b
amina <amina.yahia@eniso.u-sousse.tn>
2023-12-22 13:01:30 +0100

    Your commit message

---
+++ src/app/tree/tree.component.ts
@@ -0,0 +1,93 @@
+import { Component } from '@angular/core';
+import {NgClass, NgForOf, NgIf} from "@angular/common";
+import {MatTreeModule, MatTreeNestedDataSource} from "@angular/material/tree";
+import {NestedTreeControl} from "@angular/cdk/tree";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatCardModule} from "@angular/material/card";
+
+interface TreeNode {
+  name: string;
+  status: boolean;
+  children?: TreeNode[];
+}
+
+const fileData: TreeNode[] = [
+  {
+    name: 'css',
+    status: true,
+    children: [
+      {
+        name: 'css',
+        status: true
+      }
+    ]
+  }, {
+    name: 'html',
+    status: true,
+    children: [
+      {
+        name: 'csjss',
+        status: false
+      }
+    ]
+  },{
+    name: 'css',
+    status: true,
+    children: [
+      {
+        name: 'css',
+        status: true
+      }
+    ]
+  },
+
+];
+
+@Component({
+  selector: 'app-tree',
+  standalone: true,
+  imports: [
+
+    NgForOf,
+    NgClass,
+    MatTreeModule,
+    MatButtonModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatCardModule,
+    NgIf
+  ],
+  templateUrl: './tree.component.html',
+  styleUrl: './tree.component.css'
+})
+export class TreeComponent {
+  treeControl =new NestedTreeControl<TreeNode>(node =>node.children);
+  dataSource =new MatTreeNestedDataSource<TreeNode>();
+   dataResults:TreeNode[]=[];
+   loading:boolean=true;
+  constructor() {
+    this.dataSource.data=fileData;
+  }
+  hasChild =(_:number,node:TreeNode)=>!!node.children && node.children.length>0;
+  loadData(node: TreeNode) {
+    console.log('loadData called:', node);
+    this.dataResults.push({ name: node.name, status: node.status });
+    console.log('dataResults:', this.dataResults);
+    this.dataResults.length > 0 ? (this.loading = false) : (this.loading = true);
+  }
+
+  getStatus(status:boolean){
+    if(status){
+   return 'fishier available'
+    }
+    else {
+      return 'fishier pas available'
+    }
+  }
+  deleteFile(i:number){
+    this.dataResults.splice(i,1);
+    this.dataResults.length==0?this.loading=true :this.loading;
+  }
+}
